2f34b4615fede7b27dc3524bdc2bca257b721eec
KizievDmitriy <kizievdmitrii@gmail.com>
2022-12-28 21:25:00 +0200

    remove unnecessary

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,8 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { JsxElement } from 'typescript'
 
-export default function App({ Component, pageProps }: AppProps) {
+
+export default function App({ Component, pageProps }: AppProps): JSX.Element {
   return <Component {...pageProps} />
 }
