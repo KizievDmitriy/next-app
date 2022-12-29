9dc48893ef91b96ca3ecefdf368545028d8f3801
KizievDmitriy <kizievdmitrii@gmail.com>
2022-12-29 14:33:47 +0200

    add styles & props to btn

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -1,8 +1,10 @@
 import { ButtonProps } from "./ButtonProps"
 import s from "./Button.module.css"
 import cn from "classnames"
+import ArrowIcon from "./arrow.svg"
+// import Image from 'next/image'
 
-export const Button = ({ appearance, children, className, ...p }: ButtonProps): JSX.Element => {
+export const Button = ({ appearance, arrow = 'none', children, className, ...p }: ButtonProps): JSX.Element => {
     return (
         <button
             className={cn(s.btn, className, {
@@ -12,5 +14,8 @@ export const Button = ({ appearance, children, className, ...p }: ButtonProps): 
             {...p}
         >
             {children}
+            {arrow !== 'none' &&
+                <ArrowIcon className={cn(s.arrow, { [s.down]: arrow === 'down' })} />
+            }
         </button>);
 }
