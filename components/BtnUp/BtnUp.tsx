c1a57ce5be29e9529c8e34e59b4a601ae61c954a
KizievDmitriy <kizievdmitrii@gmail.com>
2023-01-26 16:22:16 +0200

    add btn scroll to top

---
+++ components/BtnUp/BtnUp.tsx
@@ -0,0 +1,32 @@
+import { motion, useAnimation } from "framer-motion";
+import { useEffect } from "react";
+import { useScrollY } from "../../helpers/helpers";
+import s from "./BtnUp.module.css";
+import BtnUpIcon from "./btnUp.svg"
+
+export const BtnUp = ({ ...p }): JSX.Element => {
+    const control = useAnimation();
+    const y = useScrollY();
+    useEffect(() => {
+        control.start({ opacity: y / document.body.scrollHeight })
+    }, [y, control])
+
+    const scrollToTop = () => {
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth"
+        });
+    };
+
+    return (
+        <motion.button
+            className={s.btn}
+            {...p}
+            onClick={scrollToTop}
+            animate={control}
+            initial={{ opacity: 0 }}
+        >
+            <BtnUpIcon />
+        </motion.button>
+    );
+};
